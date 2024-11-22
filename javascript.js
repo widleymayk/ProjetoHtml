76328cee8d016372169041c1149c74a5213e8a9f
Widley Mayk <widleywmss@gmail.com>
2024-11-22 19:54:59 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const paginas ={
+    "Inicio": "Inicio",
+    "sobre": "Somos uma página de serviços.",
+    "servicos": "<h1>Veja nossos serviços.</h1>",
+    "contato": "Entre em contato conosco para mais informações.",
+    
+};
+
+
+function navegarpara(pagina){
+    paginas[pagina]
+}
+
+let  conteudo = window.document.getElementById ('conteudo');
